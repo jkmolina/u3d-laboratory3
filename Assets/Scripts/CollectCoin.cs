50e2c6f60db5cce04219a09c48d48d6c7b728a1f
Joonho D. Kim <37599608+jkmolina@users.noreply.github.com>
2020-02-01 18:53:55 -0600

    first commit

---
+++ Assets/Scripts/CollectCoin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    public 
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        ScoringSystem.theScore++;
+        Destroy(gameObject);
+    }
+}
